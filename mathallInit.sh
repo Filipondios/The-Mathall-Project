6bb61ba44a071424f683e15dd070bf8c8301b15e
Filipondios <danialhamilla262@gmail.com>
2022-05-05 10:15:44 +0200

    Commit #3

---
+++ mathallInit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# If the folder that contains the project is named diferently than The-Mathall-Project be sure to rename it or it wont work.
+clear && java -jar "The-Mathall-Project/Mathall/dist/Mathall.jar"
