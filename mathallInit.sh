575502f01317ad4bee32cd36821c71097a589b41
Filipondios <danialhamilla262@gmail.com>
2022-05-05 17:30:50 +0200

    Mathall Launcher for Linux

--- mathallInit.sh
+++ mathallInit.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # If the folder that contains the project is named diferently than The-Mathall-Project be sure to rename it or it wont work.
-clear && java -jar "The-Mathall-Project/Mathall/dist/Mathall.jar"
+# Don't forget to run "chmod +x mathallInit.sh" before "./mathallInit.sh"
+
+{ # try?
+    clear && java -jar "Mathall/dist/Mathall.jar"
+} || { # catch?
+    echo -e "Mathall couldn't start due some error... \nCheck that everything is in the correct folder."
+}
