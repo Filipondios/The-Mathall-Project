0aa46a290fe4de701bb94e4962bae0901e3f9891
Filipondios <danialhamilla262@gmail.com>
2022-05-05 09:59:57 +0200

    Commit num #2

--- Mathall/src/MainPackage/prueba.java
+++ Mathall/src/MainPackage/prueba.java
@@ -3,6 +3,10 @@ package MainPackage;
 public class prueba {
 
 	public static void main(String[] args) {
+		System.out.println("prueba");
 		
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(1+1);	
+		}
 	}
 }
