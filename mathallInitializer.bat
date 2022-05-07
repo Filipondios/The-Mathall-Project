3f86194de4ab264c79da38614464b606d5dc0b1e
Filipondios <danialhamilla262@gmail.com>
2022-05-07 17:45:24 +0200

    Mathall Launcher for Windows

--- mathallInitializer.bat
+++ mathallInitializer.bat
@@ -1,8 +1,4 @@
 :: MATHALL INITIALIZER
 
-%@Try%
-  REM java -jar "Mathall\dist\Mathall.jar"
-%@EndTry%
-:@Catch
-  REM echo Mathall couldn't start due some error...Check that everything is in the correct folder.
-:@EndCatch
+java -jar "Mathall\dist\Mathall.jar"
+
