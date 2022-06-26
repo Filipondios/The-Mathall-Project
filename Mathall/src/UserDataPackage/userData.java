9c6e7003cdc4e16a8ef165bec9ba2b4006ae1c96
Filipondios <danialhamilla262@gmail.com>
2022-06-26 22:16:22 +0200

    Update at 22:08 - 26.06.2022

--- Mathall/src/UserDataPackage/userData.java
+++
@@ -1,6 +0,0 @@
-package userDataPackage;
-
-public class UserData {
-    public static String Username = System.getProperty("user.name");
-    public String OS = (System.getProperty("os.name").contains("Windows")) ? "Windows" : "Other";
-}
