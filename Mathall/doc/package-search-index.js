24f9bc132a94e24a97ed75fb3d7442246018913d
Filipondios <91225771+Filipondios@users.noreply.github.com>
2022-07-31 22:24:07 +0200

    Delete Mathall/doc directory

--- Mathall/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"algebraPackage"},{"l":"All Packages","u":"allpackages-index.html"},{"l":"filipondiosUtils"},{"l":"graphicsPackage"},{"l":"mathMainPackage"},{"l":"menusPackage"},{"l":"userDataPackage"}];updateSearchResults();
