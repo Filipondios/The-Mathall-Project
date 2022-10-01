1d2b7147195e7427ccbd0e48fe03fd1a7ad9645a
Perez Valverde Daniel <danialhamilla262@gmail.com>
2022-10-01 18:58:06 +0200

    Update graphics class

--- Mathall/src/graphicsPackage/Graphics.java
+++ Mathall/src/graphicsPackage/Graphics.java
@@ -3,48 +3,38 @@ import userDataPackage.UserData;
 /**Class that implements certain methods and variables that allow
  * certain modifications to be made to the console outputs.
  * @author Filipondios
- * @version 28.06.2022*/
+ * @version 01.10.2022*/
 public class Graphics {
 	
 	/**Object of type {@link String} that storages a token wich changes the output's
 	 * color that comes out after in the console into bold.*/
-	public String bold="\033[0;1m";
+	public static final String bold="\033[0;1m";
 	/**Object of type {@link String} that storages a token wich changes the output's
 	 * color that comes out after in the console into red.*/
-	public String red="\033[31m";
+	public static final String red="\033[31m";
 	/**Object of type {@link String} that storages a token wich changes the output's
 	 * color that comes out after in the console into white.*/
-	public String white="\033[37m";
+	public static final String white="\033[37m";
 	/**Object of type {@link String} that storages a token wich changes the output's
 	 * color that comes out after in the console into purple.*/
-	public String purple="\033[35m";
+	public static final String purple="\033[35m";
 	/**Object of type {@link String} that storages a token wich changes the output's
 	 * color that comes out after in the console into green.*/
-	public String green="\u001B[32m";
+	public static final String green="\u001B[32m";
 	/**Object of type {@link String} that storages a token wich changes the output's
 	 * color that comes out after in the console into yellow.*/
-	public String yellow="\u001B[33m";
+	public static final String yellow="\u001B[33m";
 	/**Object of type {@link String} that storages a token wich changes the output's
 	 * color that comes out after in the console into yellow.*/
-	public String blue="\033[0;34m";
-	UserData data= new UserData();
+	public static final String blue="\033[0;34m";
 	
-	/**Method that prints Mathall's logo and first menu.*/
-	public void GraphicsMenu() {
-		System.out.println(white+" |MATHALL MAIN MENU|                          "+purple+"█   █    █  █████ █   █    █    █    █");
-		System.out.println(white+" ├─["+yellow+"0"+white+"]─ Exit Mathall                          "+purple+"██ ██   █ █   █   █   █   █ █   █    █");
-		System.out.println(white+" ├─["+yellow+"1"+white+"]─ Matrix Operations                     "+purple+"█ █ █  █   █  █   █████  █   █  █    █");
-		System.out.println(white+" └─["+yellow+"2"+white+"]─ Equation Systems Resolution           "+purple+"█   █  █████  █   █   █  █████  █    █");
-		System.out.println("                                              "+purple+"█   █  █   █  █   █   █  █   █  ███  ███ @2.2.0");
-	}
-	
-	/**I method that cleans the console, and depending on the user’s OS, it does it 
+	/**Method that cleans the console, and depending on the user’s OS, it does it 
 	 * one way or another. If it is the Windows console, it will completely clean the
 	 * console if it leave a trace, but if the OS is Linux or another, it will simply
 	 * repeatedly make a backspace.*/
-	public void ClearConsole() {
+	public static void ClearConsole() {
 		try {
-			if (data.OS.equals("Windows")){
+			if (UserData.OS.equals("Windows")){
 				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
 			}
 			else{
@@ -57,11 +47,10 @@ public class Graphics {
 	/**Method that is called ONLY if the user's SO is Windows, and sets the codepage to CHCP 65001.
 	 * This is because there are special characters that needs te be printed in console and with the
 	 * default Windows codepage its not possible to represent them.*/
-	public void setCodepage() {
+	public static void setCodepage() {
 		try {
-			if (data.OS.equals("Windows")){
+			if (UserData.OS.equals("Windows"))
 				new ProcessBuilder("cmd", "/c", "CHCP 65001").inheritIO().start().waitFor();
-			}
 		} catch (Exception e) {/*Do nothing*/}
 	}
 }
