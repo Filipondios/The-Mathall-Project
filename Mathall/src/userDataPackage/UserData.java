978c76f0dafd30f657eef9112fcebd01b71b5200
Perez Valverde Daniel <danialhamilla262@gmail.com>
2022-10-02 15:01:19 +0200

    Improve user data implementation

--- Mathall/src/userDataPackage/UserData.java
+++ Mathall/src/userDataPackage/UserData.java
@@ -3,7 +3,7 @@ package userDataPackage;
  * in the {@link graphicsPackage.Graphics Graphics} class, because different implementations are required in the functions 
  * since it is not the same to give an instruction for a Linux system than for a Windows or MacOS system.
  * @author Filipondios
- * @version 26.06.2022*/
+ * @version 01.10.2022*/
 public class UserData {
 	/**String object that storages the actual username of the user's machine.*/
     public static final String Username = System.getProperty("user.name");
