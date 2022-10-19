8edaf17f978bbe6dc91bf51d36c14833e735b8e4
Filipondios <danialhamilla262@gmail.com>
2022-10-19 14:49:53 +0200

    Update Ecuations menu

--- Mathall/src/menusPackage/SistemsMenu.java
+++ Mathall/src/menusPackage/SistemsMenu.java
@@ -4,12 +4,13 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import algebraPackage.AlgebraEcuations;
 import filipondiosUtils.Print;
+import graphicsPackage.Graphics;
 import mathMainPackage.Mathallmainclass;
 /**Class that allows the user the selection of the resolution of ecuation systems. This loads a menu,
  * which waits for an integer entry that represents the option chosen by the user. In case 
  * that the entry is not correct, the user will be returned to Mathall main menu {@link Mathallmainclass}. 
  * @author Filipondios
- * @version 01.10.2022*/
+ * @version 19.10.2022*/
 public class SistemsMenu {
 
 	Scanner sc = new Scanner(System.in);
@@ -30,7 +31,8 @@ public class SistemsMenu {
 				
 				Print.prompt(); int entry = sc.nextInt(); 
 				System.out.println();
-
+				Graphics.ClearConsole();
+				
 				switch (entry) {
 					case 0:KeepModule=false;break;
 					case 1:Al.SecondGradeEquation();break;
