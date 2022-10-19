73b2fa485e1b1f3f4f65d12d4921aa902e44705c
Filipondios <danialhamilla262@gmail.com>
2022-10-19 14:50:06 +0200

    Ecuations package cleared up and organized code

--- Mathall/src/algebraPackage/AlgebraEcuations.java
+++ Mathall/src/algebraPackage/AlgebraEcuations.java
@@ -4,6 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import filipondiosUtils.FormattedNumber;
 import filipondiosUtils.MatrixUtils;
+import filipondiosUtils.Print;
 import graphicsPackage.Graphics;
 import menusPackage.SistemsMenu;
 
@@ -11,7 +12,7 @@ import menusPackage.SistemsMenu;
  * that was chosen by the user in {@link SistemsMenu}. In case that the entry in any method is
  * not correct, the user will be returned to Mathall main menu {@link SistemsMenu}. 
  * @author Filipondios
- * @version 28.06.2022*/
+ * @version 19.10.2022*/
 public class AlgebraEcuations {
 
 	Scanner sc = new Scanner(System.in);
@@ -21,13 +22,11 @@ public class AlgebraEcuations {
 	 * solution, two or none Real solutions and then solves the equation. This method dont have the option to calculte 
 	 * the imaginary solutions of equations in the case that there are not Real solutions, but it will be able to do that some day.*/
 	public void SecondGradeEquation() {
-		Gr.ClearConsole();
 		try {
-			System.out.println("█████████████████████████████████████████████████████████████████████████████████████████████\n"
-					+ "███████████████████████████ SECOND GRADE EQUATIONS RESOLUTION ███████████████████████████████\n"
-					+ "█████████████████████████████████████████████████████████████████████████████████████████████\n");			
-
-			System.out.println(" Type the equaton's value of the co-efficients a,b,c: ax\u00B2 + bx + c =0");
+			Print.note("For Mathall, a second grade equation has 3 values (a,b,c) that represents the 3 co-efficiets "
+					+ "of the equation ax\u00B2 + bx + c = 0.\n");
+			
+			System.out.println(" Type the equaton's value of the co-efficients a,b,c:");
 			System.out.print(" >>> a = "); FormattedNumber a = new FormattedNumber(sc.next());
 			System.out.print(" >>> b = "); FormattedNumber b = new FormattedNumber(sc.next());
 			System.out.print(" >>> c = "); FormattedNumber c = new FormattedNumber(sc.next());
@@ -37,7 +36,7 @@ public class AlgebraEcuations {
 
 			if(dis.doubleValue()<0) {System.out.println(" There is no real solution to this equation.\n");}
 			else {
-				b= b.product(new FormattedNumber(-1));
+				b = b.product(new FormattedNumber(-1));
 
 				if (Math.sqrt(dis.doubleValue())- (int) (Math.sqrt(dis.doubleValue()))!=0) {
 					String chain1 = b+"+\u221A"+dis+"";
@@ -65,33 +64,28 @@ public class AlgebraEcuations {
 					FormattedNumber result2 = a.divisionOf(b.substract(dis), a.product(new FormattedNumber(2)));
 
 					if (result1.equals(result2)) {
-						System.out.println(" There is a double solution to this equation: \nx= "+result1+"\n");
+						System.out.println(" There is a double solution to this equation: \n x= "+result1+"\n");
 					}else {
-						System.out.println(" There are two solutions for this equation: \nx1= "+result1+"\nx1= "+result2+"\n");
+						System.out.println(" There are two solutions for this equation: \n x1= "+result1+"\n x2= "+result2+"\n");
 					}
 				}
 			}
 
 		}catch (InputMismatchException e) {
 			sc.nextLine();
-			System.out.println(Gr.red+" Mathall-System."+Gr.white+"output~#: Check your input: its not accepted.\n");
+			Print.warning("Check your input: its not accepted.\n");
 		}
 	}
 
 	/**Method that calculates the solutions of linear equation systems. It tells the user if there are infinite solutions,
 	 * a determinated number of solutions or none.*/
 	public void ecuationSystems() {
-		Gr.ClearConsole();
 		try {
-			System.out.println("█████████████████████████████████████████████████████████████████████████████████████████████\n"
-					+ "███████████████████████████ LINEAR ECUATION SYSTEMS RESOLUTION ██████████████████████████████\n"
-					+ "█████████████████████████████████████████████████████████████████████████████████████████████\n");			
-
-			System.out.println(Gr.yellow+" * "+Gr.green+"NOTE:"+Gr.white+" For Mathall, a linear ecuation system consists on n ecuations with n variables. "
-					+ Gr.yellow+"\n * "+Gr.white+"For example, a 3 ecuations system with 3 variables looks like:\n"+Gr.yellow+" *"
-					+ "\n * "+Gr.white+" | ax + by +cz = d\n"+Gr.yellow+" * "+Gr.white+"-| a'x + b'y +c'z = d'\n"+Gr.yellow+" * "+Gr.white+" | a''x + b''y +c''z = d''\n"+Gr.yellow+" *"
-					+ Gr.yellow+"\n * "+Gr.green+"NOTE:"+Gr.white+ "a,b,c are constants (numbers) and x,y,z are variables.");
-			
+			Print.note("For Mathall, a linear ecuation system consists on n ecuations with n variables. "
+					+"For example, a 3 ecuations system with 3 variables looks like:\n");
+			System.out.println("  | ax + by +cz = d\n -| a'x + b'y +c'z = d'\n  | ax'' + b''y + c''z = d''");
+			Print.note("a,b,c are constants (numbers) and x,y,z are variables.");		
+						
 			System.out.print("\n Now, type the number of ecuations and variables: ");
 			int Dimension = sc.nextInt();
 			System.out.println("\n Type the ecuation system data:\n");
@@ -127,10 +121,10 @@ public class AlgebraEcuations {
 				int RankA = mu.matrixRank(A);
 				
 				if (RankA == RankSystem) {
-					System.out.println(" The Equation System has infinite solutions.");
+					Print.result("The Equation System has infinite solutions.\n");
 				}else {
-					System.out.println(" The Equation System has no solution"
-							+ "or is not a "+Dimension+" variable ecuation system :(\n"); 
+					Print.result("The Equation System has no solution"
+					+ "or is not a "+Dimension+" variable ecuation system :(\n");
 				}
 			}
 			else {
@@ -149,19 +143,20 @@ public class AlgebraEcuations {
 						A[j][i] = tempCol[j];
 				}
 				
-				System.out.print(" The System is determinate compatible. The solutions are:\n (");
+				Print.result("The System is determinate compatible. The solutions are:");
+				System.out.print(" (");
 				for (int i = 0; i < solutions.length; i++) {
 					if (i!=solutions.length-1) { System.out.print(solutions[i]+", "); }
 					else System.out.print(solutions[i]+")\n");
 				}
-				System.out.println(Gr.yellow+"\n * "+Gr.green+"NOTE:"+Gr.white+"The solutions are written in the next format: If you have a"
-						+ Gr.yellow+"\n * "+Gr.white+"system with 2 variables, the solution will be (x,y), if you have a 3\n"
-						+ Gr.yellow+" * "+Gr.white+"variables system, the solution will be (x,y,z) and so on...\n");
+				Print.note("The solutions are written in the next format: If you have a system with 2 variables,"
+						+ " the solution will be (x,y), if you have a 3 variables system, the solution will be " 
+						+ "(x,y,z) and so on...\n");
 			}
 			
 		}catch (InputMismatchException e) {
 			sc.nextLine();
-			System.out.println(Gr.red+" Mathall-System."+Gr.white+"output~#: Check your input: its not accepted.\n");
+			Print.warning("Check your input: its not accepted.\n");
 		}
 	}
 }
