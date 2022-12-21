176456548e748516c007205664ff822393924c28
Filipondios <danialhamilla262@gmail.com>
2022-12-21 16:55:48 +0100

    Delete Mathall-Java

--- Mathall-Java (Discontinued)/src/filipondiosUtils/NoInverseMatrixExists.java
+++
@@ -1,8 +0,0 @@
-package filipondiosUtils;
-
-@SuppressWarnings("serial")
-public class NoInverseMatrixExists extends Exception{
-	public NoInverseMatrixExists(String message) {
-		System.out.println(message);
-	}
-}
