05a41e767881847d767222fa24d190a1c744337b
Filipondios <danialhamilla262@gmail.com>
2022-12-21 16:38:01 +0100

    Main

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,9 @@ fn main() {
      *   4  5   6
      *   7  8   9
      * As a vector of vectos of f32.*/
-    let vect2: Vec<Vec<f32>> = vec![vec![1.0, 2.0, 3.0], vec![4.0, 5.0, 6.0], vec![7.0, 8.0, 9.0]];
+    let vect2: Vec<Vec<f32>> = vec![vec![5.0, 0.0, 0.0], vec![0.0, 2.0, 0.0], vec![0.0, 0.0, 7.0]];
 
-    let mut result: Vec<Vec<f32>> = matrices::matrix_traspose(&vect1);
+    let result: Vec<Vec<f32>> = matrices::matrix_inv(&vect2);
     matrices::matrix_print(&result);
-    println!();
-    result = matrices::matrix_sum(&vect1, &vect2);
-    matrices::matrix_print(&result);
-    println!();
-    result = matrices::matrix_sub(&vect1, &vect2);
-    matrices::matrix_print(&result);
-    println!();
-    result = matrices::matrix_mult(&vect1, &vect2);
-    matrices::matrix_print(&result);
-    println!();
+    //println!("El determinante es: {}", result);
 }
